7c0d6287bfce46da36aa6b784c72b694c4ab067e
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-11-30 22:08:34 -0600

    most functions working, i think

--- lib/ext2_access.c
+++ lib/ext2_access.c
@@ -17,23 +17,30 @@
 
 // Return a pointer to the primary superblock of a filesystem.
 struct ext2_super_block * get_super_block(void * fs) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_super_block(fs);
+ //   return _ref_get_super_block(fs);
+
+    return (struct ext2_super_block *) (fs + SUPERBLOCK_OFFSET);
 }
 
 
 // Return the block size for a filesystem.
-__u32 get_block_size(void * fs) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_block_size(fs);
+__u32 get_block_size(void * fs) { 
+//    return _ref_get_block_size(fs);
+
+    struct  ext2_super_block* super_block = get_super_block(fs);
+    //IS THIS OFFSET OR SIZE
+    return SUPERBLOCK_SIZE << super_block->s_log_block_size;
 }
 
 
 // Return a pointer to a block given its number.
 // get_block(fs, 0) == fs;
 void * get_block(void * fs, __u32 block_num) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_block(fs, block_num);
+//    return _ref_get_block(fs, block_num);
+
+    __u32 size_of_block = get_block_size(fs);
+    __u32 total_offset = size_of_block * block_num;
+    return fs + total_offset;
 }
 
 
@@ -41,8 +48,12 @@ void * get_block(void * fs, __u32 block_num) {
 // ext2 filesystems will have several of these, but, for simplicity, we will
 // assume there is only one.
 struct ext2_group_desc * get_block_group(void * fs, __u32 block_group_num) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_block_group(fs, block_group_num);
+  //  return _ref_get_block_group(fs, block_group_num);
+
+    __u32 size_of_block = get_block_size(fs);
+    __u32 background_block = (SUPERBLOCK_OFFSET / size_of_block) + 1;
+    void* block_group_ptr = get_block(fs, background_block); 
+    return (struct ext2_group_desc *) block_group_ptr; 
 }
 
 
@@ -50,8 +61,12 @@ struct ext2_group_desc * get_block_group(void * fs, __u32 block_group_num) {
 // would require finding the correct block group, but you may assume it's in the
 // first one.
 struct ext2_inode * get_inode(void * fs, __u32 inode_num) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_inode(fs, inode_num);
+   // return _ref_get_inode(fs, inode_num);
+
+    struct ext2_group_desc* background_group = get_block_group(fs, 0);
+    __u32 block_with_inode = background_group->bg_inode_table;
+    struct ext2_inode* inode = get_block(fs, block_with_inode);
+    return inode + inode_num - 1;
 }
 
 
@@ -98,12 +113,38 @@ struct ext2_inode * get_root_dir(void * fs) {
 
 // Given the inode for a directory and a filename, return the inode number of
 // that file inside that directory, or 0 if it doesn't exist there.
-// 
 // name should be a single component: "foo.txt", not "/files/foo.txt".
-__u32 get_inode_from_dir(void * fs, struct ext2_inode * dir, 
-        char * name) {
-    // FIXME: Uses reference implementation.
-    return _ref_get_inode_from_dir(fs, dir, name);
+
+__u32 get_inode_from_dir(void * fs, struct ext2_inode * dir, char * name) {
+  //   return _ref_get_inode_from_dir(fs, dir, name);
+
+    if (!(LINUX_S_ISDIR(dir->i_mode))) {
+        return 0;
+    };
+
+    __u32 size_of_block = get_block_size(fs);
+    void* block_directory = get_block(fs, dir->i_block[0]);
+    struct ext2_dir_entry* node = (struct ext2_dir_entry *) block_directory;
+    void* end = ((void *) node) + size_of_block;
+
+    __u32 target_inode = 0;
+    while ((void*) node < end) {
+
+        if (node->inode != 0) {
+            if ((strlen(name) == (unsigned char) (node->name_len)) && (strncmp(name, node->name, strlen(name))) == 0) {
+                target_inode = node->inode;
+            }
+            node = (struct ext2_dir_entry *) (((void*) node) + node->rec_len);
+        }
+    }
+
+    if (target_inode != 0) {
+        return target_inode;
+    } 
+    else {
+        return 0;
+    }
+
 }
 
 
